856f4d5dd942cf69b9090440fa3cdc909a0c76d7
Nordveyl <kkrutoychel.wf@gmail.com>
2021-11-03 02:31:21 +0300

    Update 3.rb

--- 3.rb
+++ 3.rb
@@ -9,5 +9,16 @@
 # - Результат должен быть выведен в консоль командой puts
 #
 ## Решение:
+kod = 0
+file = File.open('data/3.txt')
+#mas = [] 
+IO.foreach(file) do |line|
+	mas = line.split(' ').map { |n| n.to_i } 
+	razn = mas.max - mas.min  
+	kod +=razn 
+	mas.clear
+end
+puts kod 
 
+ 
 
